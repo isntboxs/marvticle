bfa3193c07d26451fa8688a5a40cdc6546f547bb
mrboxs <rizkywanda200155@gmail.com>
2026-04-21 14:21:15 +0700

    feat: add nitro and update environment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,20 @@ import { devtools } from '@tanstack/devtools-vite'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
+import { nitro } from 'nitro/vite'
+
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [
+    devtools(),
+    nitro({ preset: 'bun' }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
 })
 
 export default config
